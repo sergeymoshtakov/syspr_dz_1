7f393aa1a5bd2ebd8823681a6002bab5119b6b19
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-09-14 19:52:55 +0200

    Add files via upload

---
+++ SPNP/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPNP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ThreadingButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new ThreadingWindow().ShowDialog();
+            this.Show();
+        }
+    }
+}
